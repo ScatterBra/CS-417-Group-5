c44b37c711d9bbb7ab6c1a7978b0b253c6352a75
Zheng <hjzheng@illinois.edu>
2026-09-23 21:50:59 -0500

    Save work before rebase

--- Assets/Scripts/Drawer.cs
+++ Assets/Scripts/Drawer.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Drawer : MonoBehaviour
 {
+    [SerializeField] GameObject drawer;
     [SerializeField] private Transform drawerTransform;
     [SerializeField] private bool isOpen = false;
 
+    [Header("Drawer LockingSettings")]
     [SerializeField] private bool isLocked = false;
     [SerializeField] private GameObject key;    // If the drawer is locked, this will be the key object that can unlock it
-    [SerializeField] private GameObject lock;   // If the drawer is locked, this will be the lock object that is displayed on the drawer
+    [SerializeField] private GameObject lockForKey;   // If the drawer is locked, this will be the lock object that is displayed on the drawer
     [SerializeField] private InputActionReference openDrawerAction; // The input action that will be used to open the drawer
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         drawerTransform = GetComponent<Transform>();
+        
     }
 
     
