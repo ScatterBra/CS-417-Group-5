1a0256e817a3ccb153d8c0ba13e1517fa54c8388
Hansen <zhansen1318@gmail.com>
2026-09-23 21:00:39 -0500

    Start script for drawers, import some assets

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+    [SerializeField] private Transform drawerTransform;
+    [SerializeField] private bool isOpen = false;
+
+    [SerializeField] private bool isLocked = false;
+    [SerializeField] private GameObject key;    // If the drawer is locked, this will be the key object that can unlock it
+    [SerializeField] private GameObject lock;   // If the drawer is locked, this will be the lock object that is displayed on the drawer
+    [SerializeField] private InputActionReference openDrawerAction; // The input action that will be used to open the drawer
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        drawerTransform = GetComponent<Transform>();
+    }
+
+    
+}
